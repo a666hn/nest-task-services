1aece45e9351494c34a9d881869ec1c73cc680c8
Adi Hermawan <adidot78@gmail.com>
2021-06-05 21:16:11 +0700

    now filter by search can't be empty field

--- src/tasks/dto/filter-task.dto.ts
+++ src/tasks/dto/filter-task.dto.ts
@@ -1,8 +1,9 @@
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 import { TASK_STATUS } from 'src/globals/constant/constant';
 import { ETaskStatus } from '../interface/tasks.interface';
 
 export class FilterTaskDto {
+  @IsNotEmpty()
   search: string;
 
   @IsEnum(ETaskStatus, { message: `The status must be one of "${Object.keys(TASK_STATUS).join(', ')}"` })
