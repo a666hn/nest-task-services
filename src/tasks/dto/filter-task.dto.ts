8400542bc0c1b1eed62610b9d038533797b2f9a6
Adi Hermawan <adidot78@gmail.com>
2021-06-05 21:18:03 +0700

    Change validation filter task

--- src/tasks/dto/filter-task.dto.ts
+++ src/tasks/dto/filter-task.dto.ts
@@ -1,11 +1,13 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TASK_STATUS } from 'src/globals/constant/constant';
 import { ETaskStatus } from '../interface/tasks.interface';
 
 export class FilterTaskDto {
-  @IsNotEmpty()
+  @IsOptional()
+  @IsString()
   search: string;
 
+  @IsOptional()
   @IsEnum(ETaskStatus, { message: `The status must be one of "${Object.keys(TASK_STATUS).join(', ')}"` })
   status: ETaskStatus;
 }
