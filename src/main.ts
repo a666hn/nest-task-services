cab552bc3465ff52e5ef8d72bdcefeeaef8fc52c
Adi Hermawan <adidot78@gmail.com>
2021-06-05 19:44:47 +0700

    Add validation in main level; validate create new task;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.use(cors());
   app.setGlobalPrefix('/api');
