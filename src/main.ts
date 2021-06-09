6d30a3635113790bbca83f559e090288aa33abe9
Adi Hermawan <adidot78@gmail.com>
2021-06-09 19:32:16 +0700

    Change binding port dev to env

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +14,6 @@ async function bootstrap() {
   app.use(cors());
   app.setGlobalPrefix('/api');
 
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
